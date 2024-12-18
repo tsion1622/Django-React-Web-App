baeaea6ca3d09982a5b4c8fcf9f0532c9c6cd533
Tsion Tamirat <tayu.16.enye@gmail.com>
2024-12-18 23:36:18 +0300

    Created NotFound functional component to display a user-friendly message for non-existent pages

--- new-frontend/src/pages/NotFound.jsx
+++ new-frontend/src/pages/NotFound.jsx
@@ -1,5 +1,10 @@
 function NotFound () {
-    return <div>Not Found</div>
+    return <div>
+        <h1>Not Found</h1>
+
+<p>The Page You are looking for does not exist!</p>
+
+    </div>
 }
 
 export default NotFound 
